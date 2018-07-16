941fb847dd36a76521e2c9f6aed196b621abf53c
Erin <erinedawson@gmail.com>
2018-07-16 15:28:37 -0600

    add toggle functionality to buttons

--- script.js
+++ script.js
@@ -2,17 +2,55 @@ let filler ='Flannel meh palo santo 8-bit, hammock tumblr pok pok chicharrones b
 
 let words = filler.split(' ');
 
-let totalWords = function(){
-  document.getElementById('total').innerHTML = `There are ${words.length} words in this paragraph.`;
+let t = document.getElementById('total');
+let d = document.getElementById('dupe');
+let s = document.getElementById('sentence');
+
+let totalWords = () => {
+
+  t.innerHTML = `There are ${words.length} words in this paragraph.`
 };
+
+let toggle1 = () => {
+  if(t.style.color != 'black'){
+    t.style.color = 'black';
+  }else{
+    t.style.color = 'white';
+  }
+}
+
+
+
+let todos1 = () => {
+  totalWords();
+  toggle1();
+}
+
+let todos2 = () => {
+  duplicateWords();
+  toggle2();
+}
+
+let todos3 = () => {
+  sentCount();
+  toggle3();
+}
 
 let dupes = 'art party tousled palo santo craft beer flannel pork belly shaman authentic mlkshk yr hexagon meh tumblr fixie street biodiesel gentrify tacos selvage pok bitters quinoa semiotics slow-carb migas chillwave you probably haven\'t heard of them williamsburg plaid tbh pop-up whatever'
 
 let dWords = dupes.split(' ');
 
 let duplicateWords = () => {
-  document.getElementById('dupe').innerHTML = `There are ${dWords.length} duplicated words in this paragraph.`;
-  console.log(dWords.length);
+  d.innerHTML = `There are ${dWords.length} duplicated words in this paragraph.`;
+
+}
+
+let toggle2 = () => {
+  if(d.style.color != 'black'){
+    d.style.color = 'black';
+  }else{
+    d.style.color = 'white';
+  }
 }
 
 let sentenceCount = 0;
@@ -23,5 +61,13 @@ let sentCount = () => {
       sentenceCount++;
     }
   })
-  document.getElementById('sentence').innerHTML = `There are ${sentenceCount} sentences in this paragraph.`
+  s.innerHTML = `There are ${sentenceCount} sentences in this paragraph.`
+}
+
+let toggle3 = () => {
+  if(s.style.color != 'black'){
+    s.style.color = 'black';
+  }else{
+    s.style.color = 'white';
+  }
 }
