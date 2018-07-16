b43fdb118987db938174f8800352a7d781fcefc9
Erin <erinedawson@gmail.com>
2018-07-16 10:15:44 -0600

    write totalWords function

---
+++ script.js
@@ -0,0 +1,6 @@
+let filler ='Flannel meh palo santo 8-bit, hammock tumblr pok pok chicharrones bitters pop-up quinoa semiotics shabby chic. Food truck street art biodiesel, narwhal quinoa lumbersexual cray. Farm-to-table hexagon godard, prism seitan slow-carb migas chillwave readymade enamel pin cold-pressed selvage you probably haven\'t heard of them. Man braid retro austin church-key williamsburg gastropub plaid chia jean shorts single-origin coffee gentrify hell of. Pork belly tbh health goth dreamcatcher pop-up migas tote bag you probably haven\'t heard of them whatever. Keffiyeh vice schlitz whatever, iPhone edison bulb craft beer vaporware williamsburg irony fixie letterpress. Pinterest bespoke humblebrag shaman semiotics.'
+
+let words = filler.split(' ');
+let totalWords = words.length;
+
+console.log(`there are ${totalWords} words in this paragraph.`);
